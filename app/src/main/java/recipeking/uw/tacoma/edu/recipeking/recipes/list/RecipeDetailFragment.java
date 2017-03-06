03c2eb6e48b454696e67d2e407f6869b4691d4ca
George <gestojanov@gmail.com>
2017-03-06 12:17:21 -0800

    Added rating functionality

--- app/src/main/java/recipeking/uw/tacoma/edu/recipeking/recipes/list/RecipeDetailFragment.java
+++ app/src/main/java/recipeking/uw/tacoma/edu/recipeking/recipes/list/RecipeDetailFragment.java
@@ -5,7 +5,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -28,6 +28,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import recipeking.uw.tacoma.edu.recipeking.MainActivity;
 import recipeking.uw.tacoma.edu.recipeking.R;
@@ -46,9 +48,17 @@ public class RecipeDetailFragment extends Fragment {
     private final static String ADD_COMMENT_URL =
             "http://cssgate.insttech.washington.edu/~_450bteam7/addComment.php?";
 
+    /** String constant for the add rating URL */
+    private final static String ADD_RATING_URL =
+            "http://cssgate.insttech.washington.edu/~_450bteam7/addRating.php?";
+
     /** String constant for the comments list URL */
     private final static String COMMENTS_LIST_URL =
             "http://cssgate.insttech.washington.edu/~_450bteam7/comments_list.php?";
+
+    /** String constant for the ratings get URL */
+    private final static String GET_RATINGS_URL =
+            "http://cssgate.insttech.washington.edu/~_450bteam7/ratings.php?";
 
     /** TextView element for the recipe's title. */
     private TextView mRecipeTitle;
@@ -77,7 +87,11 @@ public class RecipeDetailFragment extends Fragment {
     /** A button for submitting a new comment. */
     private Button mAddCommentButton;
 
-    private ShareActionProvider mShareActionProvider;
+    private EditText mEditTextRating;
+
+    private TextView mRatings;
+
+    private Button mAddRatingButton;
 
 
     /**
@@ -136,6 +150,10 @@ public class RecipeDetailFragment extends Fragment {
         mComments = (TextView) view.findViewById(R.id.recipe_comments_tv);
         mEditTextComment = (EditText) view.findViewById(R.id.add_new_comment_et);
         mAddCommentButton = (Button) view.findViewById(R.id.add_new_comment_button);
+
+        mRatings = (TextView) view.findViewById(R.id.ratings_tv);
+        mEditTextRating = (EditText) view.findViewById(R.id.add_new_rating_et);
+        mAddRatingButton = (Button) view.findViewById(R.id.add_new_rating_button);
 
         return view;
     }
@@ -276,7 +294,70 @@ public class RecipeDetailFragment extends Fragment {
 
                 }
             });
+
+            //RETRIEVE RATINGS FOR THIS RECIPE
+            getRatingsForThisRecipe();
+
+            //SET THE SUBMIT BUTTON LISTENER FOR ADDING NEW RATING
+            mAddRatingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean invalid = false;
+                    if (mEditTextRating.getText().toString().isEmpty()
+                            || !isRatingFieldValid()) {
+                        invalid = true;
+                    }
+
+                    if(invalid) {
+                        mEditTextRating.setError("Try again");
+                        mEditTextRating.requestFocus();
+                    } else {
+                        //ASYNC for adding a rating
+                        String url = buildAddRatingUrl();
+                        AddRatingTask task = new AddRatingTask();
+                        task.execute(new String[]{url});
+                    }
+                }
+            });
         }
+    }
+
+    /**
+     * Validates the EditText field for specifying a rating. Valid values (1,2,3,4,5)
+     * @return - true if valid value is specified, false otherwise.
+     */
+    private boolean isRatingFieldValid() {
+        boolean valid = false;
+        String value = mEditTextRating.getText().toString();
+        if (value.length() <= 1 && isValidInteger(value)) {
+            int i = Integer.parseInt(value);
+            if (i >= 1 && i <= 5) {
+                valid = true;
+            }
+        }
+
+        return valid;
+    }
+
+    /**
+     * Checks if a string can represent an positive integer
+     *
+     * @param theInput the string to parse
+     *
+     * @return true if the string can represent a positive integer, false otherwise.
+     */
+    private boolean isValidInteger(String theInput) {
+        boolean result = false;
+        try {
+            int val = Integer.parseInt(theInput);
+            if (val > 0) {
+                result = true;
+            }
+        } catch (Exception e) {
+            result = false;
+        }
+
+        return result;
     }
 
     /**
@@ -285,6 +366,15 @@ public class RecipeDetailFragment extends Fragment {
     private void getCommentsForThisRecipe() {
         String url = buildCommentsListUrl();
         DownloadCommentsForThisRecipe task = new DownloadCommentsForThisRecipe();
+        task.execute(new String[]{url});
+    }
+
+    /**
+     * Downloads the ratings for this recipe from CSS Gate.
+     */
+    private void getRatingsForThisRecipe() {
+        String url = buildRatingsListUrl();
+        DownloadRatingsForThisRecipe task = new DownloadRatingsForThisRecipe();
         task.execute(new String[]{url});
     }
 
@@ -310,7 +400,35 @@ public class RecipeDetailFragment extends Fragment {
 
         } catch (Exception e) {
             Toast.makeText(getActivity(),
-                    "Something wrong with the AddFavlist url" + e.getMessage(),
+                    "Something wrong with the AddComment url" + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Builds the AddRatingUrl for adding a rating of this recipe to the server.
+     * @return - a string.
+     */
+    private String buildAddRatingUrl() {
+        StringBuilder sb = new StringBuilder(ADD_RATING_URL);
+
+        try {
+            String title = mRecipeTitle.getText().toString();
+            sb.append("title=");
+            sb.append(URLEncoder.encode(title, "UTF-8"));
+
+            sb.append("&user=");
+            sb.append(URLEncoder.encode(MainActivity.currentUser, "UTF-8"));
+
+            sb.append("&rating=");
+            sb.append(URLEncoder.encode(mEditTextRating.getText().toString(), "UTF-8"));
+
+
+        } catch (Exception e) {
+            Toast.makeText(getActivity(),
+                    "Something wrong with the AddRating url" + e.getMessage(),
                     Toast.LENGTH_LONG).show();
         }
 
@@ -338,6 +456,27 @@ public class RecipeDetailFragment extends Fragment {
         return sb.toString();
     }
 
+    /**
+     * Builds the GET url for downloading recipe ratings from the server.
+     * @return - a string.
+     */
+    private String buildRatingsListUrl() {
+        StringBuilder sb = new StringBuilder(GET_RATINGS_URL);
+
+        try {
+            String title = mRecipeTitle.getText().toString();
+            sb.append("title=");
+            sb.append(URLEncoder.encode(title, "UTF-8"));
+
+        } catch (Exception e) {
+            Toast.makeText(getActivity(),
+                    "Something wrong with the GetRatings url" + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return sb.toString();
+    }
+
 
     /**
      * Parses the JSON comments data from the CSS GATE server and fills the mCommentsString field
@@ -358,6 +497,49 @@ public class RecipeDetailFragment extends Fragment {
                     String comment = obj.getString("usr_comment");
 
                     mCommentsString = mCommentsString + comment + "\n";
+                }
+
+            } catch (JSONException e) {
+                reason = "Unable to parse data, Reason: " + e.getMessage();
+            }
+        }
+
+        return reason;
+    }
+
+    /**
+     * Parses the JSON rating data from the CSS GATE server and fills the ratings TextView field
+     * with the ratings.
+     * @param ratingsJSON - the retrieved JSON data.
+     * @return - If null it is successful, else something went wrong.
+     */
+    private String parseRatingsJSON(String ratingsJSON) {
+        String reason = null;
+        List<Integer> ratingList = new ArrayList<Integer>();
+
+        if (ratingsJSON != null) {
+            try {
+                JSONArray arr = new JSONArray(ratingsJSON);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject obj = arr.getJSONObject(i);
+                    int rating = Integer.parseInt(obj.getString("rating"));
+
+                    ratingList.add(rating);
+                }
+
+                if (!ratingList.isEmpty()) {
+                    double sum = 0;
+                    for (int i : ratingList) {
+                        sum += i;
+                    }
+                    double avg = sum / ratingList.size();
+                    System.out.println(avg);
+                    Log.i("parseRatingsJSON avg:", String.valueOf(avg));
+                    Log.i("parseRatingsJSON sum:", String.valueOf(sum));
+                    Log.i("parseRatingsJSON size:", String.valueOf(ratingList.size()));
+                    String result = String.format("Ratings (%d): %.2f", ratingList.size(), avg);
+                    mRatings.setText(result);
                 }
 
             } catch (JSONException e) {
@@ -441,6 +623,73 @@ public class RecipeDetailFragment extends Fragment {
         }
     }
 
+    /**
+     * AsyncTask for downloading the ratings for the current recipe. Sets the ratings TextView
+     * field with the ratings received.
+     */
+    private class DownloadRatingsForThisRecipe extends AsyncTask <String, Void, String> {
+
+        /**
+         * doInBackground() method for this task. Downloads the ratings from the CSS Gate server.
+         * @param urls - GET RATINGS Url.
+         * @return - the resulting JSON data.
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuilder result = new StringBuilder();
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        result.append(s);
+                    }
+                } catch (Exception e) {
+                    result = new StringBuilder("Unable to get ratings for this recipe, Reason: "
+                            + e
+                            .getMessage());
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+            return result.toString();
+        }
+
+        /**
+         * onPostExecute() method for this task. Fills the ratings TextView field with the
+         * ratings retrieved.
+         * @param result - the retrieved JSON data.
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            if (!result.contains("No ratings found.")) {
+                // Fills the mCommentsString with the comments parsed.
+                result = parseRatingsJSON(result);
+
+                if(result != null) {
+                    Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+            }
+
+        }
+    }
+
 
     /**
      * AsyncTask for adding a comment for the current recipe. Sends the user specified comment
@@ -470,7 +719,7 @@ public class RecipeDetailFragment extends Fragment {
                         result.append(s);
                     }
                 } catch (Exception e) {
-                    result = new StringBuilder("Unable to add recipe to favorites list, Reason: "
+                    result = new StringBuilder("Unable to add comment to server, Reason: "
                             + e
                             .getMessage());
                 } finally {
@@ -495,6 +744,70 @@ public class RecipeDetailFragment extends Fragment {
                         .show();
 
                 mEditTextComment.setText("");
+
+                // Refresh view for this fragment, so the comment will be visible immediately
+                Bundle args = getArguments();
+                if (args != null) {
+                    updateView((Recipe) args.getSerializable(RECIPE_ITEM_SELECTED));
+                }
+            }
+        }
+
+    }
+
+    /**
+     * AsyncTask for adding a rating for the current recipe. Sends the user specified rating
+     * to the server.
+     */
+    private class AddRatingTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * doInBackground() method for this task.
+         * @param urls
+         * @return
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+
+            StringBuilder result = new StringBuilder();
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        result.append(s);
+                    }
+                } catch (Exception e) {
+                    result = new StringBuilder("Unable to add rating to server, Reason: "
+                            + e
+                            .getMessage());
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+            return result.toString();
+
+        }
+
+        /**
+         * onPostExecute method for this task.
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.contains("success")) {
+                Toast.makeText(getActivity().getApplicationContext(), "Rating submitted.", Toast
+                        .LENGTH_SHORT)
+                        .show();
+
+                mEditTextRating.setText("");
 
                 // Refresh view for this fragment, so the comment will be visible immediately
                 Bundle args = getArguments();
